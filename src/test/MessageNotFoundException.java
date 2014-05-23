9767d60bc353df8a1955b6a6f94e4343707dcf1c
yasserAMuhammed <yasser.csd@gmail.com>
2014-05-23 15:56:21 +0300

    Adding interfaces and resources

---
+++ src/test/MessageNotFoundException.java
@@ -0,0 +1,15 @@
+package test;
+public class MessageNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	private int[] msgNum = null;
+
+	public int[] getMsgNum() {
+		return msgNum;
+	}
+
+	public void setMsgNum(int[] msgNum) {
+		this.msgNum = msgNum;
+	}
+
+}
